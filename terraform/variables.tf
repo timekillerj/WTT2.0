b33ead8c48e16c883dd177bd19c1641a10527a2d
Jason Donahue <jdonahue@timekiller.org>
2026-05-09 02:57:27 -0400

    remove unused variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "s3_bucket_name" {
   default = "techtask-public-mongo-backups"
 }
